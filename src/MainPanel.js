5c16168471cdabfbaaedce62971337855f7f5855
bandieta <michal.bandrowski@gmail.com>
2023-10-06 13:07:44 +0200

    Selected shape added

--- src/MainPanel.js
+++ src/MainPanel.js
@@ -1,14 +1,26 @@
 import ShapeList from "./ShapeList";
-
+import React, { useEffect, useState } from 'react';
 
 export default function MainPanel(){
+
+    const [selectedShape, setSelectedShape] = useState(true);
+
+    useEffect(() => {
+        // Use the data imported from the JSON file
+        setSelectedShape("QDa")
+      }, []);
+
+    function handleShapeChange(e){
+        setSelectedShape(e)
+    }
+
     return <div>
         <h1>Alnor Cam</h1>
 
-
-        <ShapeList/>
+        <ShapeList selectedShape={selectedShape} setSelectedShape={handleShapeChange}/>  
         
     </div>
 }
+
 
 
--- src/ShapeList.js
+++ src/ShapeList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import data from './ShapesList.json'; // Adjust the path as needed
 import './ShapeList.css';
 
-function ShapeList() {
+function ShapeList({selectedShape, setSelectedShape}) {
   const [list, setList] = useState([]);
 
   useEffect(() => {
@@ -16,8 +16,10 @@ function ShapeList() {
       <ul className="shape-list">
         {list.map((shape, index) => (
           <li key={index}>
-             <button className="shape-button">
-              <strong>{shape.symbol}</strong> {shape.name}
+             <button className="shape-button" onClick={() => setSelectedShape(shape.symbol)}>
+              <strong>
+                {shape.symbol === selectedShape ? '['+shape.symbol+']' : shape.symbol}
+                </strong> {shape.name}
             </button>
           </li>
         ))}
